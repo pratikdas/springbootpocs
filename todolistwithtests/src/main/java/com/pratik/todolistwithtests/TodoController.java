d463080ac64b81e2e520cf0f3fa2fd46c5670821
Pratik Das <pratikdas@Pratiks-MacBook-Pro.local>
2020-04-21 10:38:31 +0400

    added post method and tests

--- todolistwithtests/src/main/java/com/pratik/todolistwithtests/TodoController.java
+++ todolistwithtests/src/main/java/com/pratik/todolistwithtests/TodoController.java
@@ -4,6 +4,8 @@
 package com.pratik.todolistwithtests;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,14 @@ public class TodoController {
 	@GetMapping("/randomtodo")
 	public String fetchAnyTodoItem(@RequestParam(name = "dayofweek", defaultValue = "sunday") final String dayOfWeek) {
 		return String.format("my random todo of %s is workout", dayOfWeek);
+	}
+	
+	@PostMapping("/addTodo")
+	public TodoItem createTodoItem(@RequestBody TodoItem todoItem) {
+		
+		TodoItem item = new TodoItem();
+		
+		return item;
 	}
 
 }
--- todolistwithtests/src/test/java/com/pratik/todolistwithtests/TodoControllerTest.java
+++ todolistwithtests/src/test/java/com/pratik/todolistwithtests/TodoControllerTest.java
@@ -3,22 +3,23 @@
  */
 package com.pratik.todolistwithtests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 
@@ -45,6 +46,24 @@ class TodoControllerTest {
 	@Test
 	void fetchAnyTodoItem_for_dayofweek() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/randomtodo?dayofweek=monday")).andExpect(content().string("my random todo of monday is workout"));
+	}
+	
+	@Test
+	void createTodoItem_with_valid_data() throws Exception {
+		TodoItem todoItem = new TodoItem();
+		todoItem.setId("uyyyu");
+		
+		mvc.perform(MockMvcRequestBuilders.post("/create")
+				    .content(asJson(todoItem))
+				    .contentType(MediaType.APPLICATION_JSON)
+				    .accept(MediaType.APPLICATION_JSON))
+		            .andExpect(MockMvcResultMatchers.status().isCreated())
+		            .andExpect(MockMvcResultMatchers.jsonPath("$.name").exists());
+	}
+	
+	private String asJson(TodoItem todoItem ) throws JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.writeValueAsString(todoItem);
 	}
 
 }
